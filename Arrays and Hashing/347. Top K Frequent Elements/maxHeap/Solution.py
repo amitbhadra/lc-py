0be758dd5b13d9faf9d2ba63e2a556f3adfc29fc
Amit Bhadra <amitbhadra7@gmail.com>
2023-11-19 21:00:02 -0800

    11_19_2023

--- Arrays and Hashing/347. Top K Frequent Elements/maxHeap/Solution.py
+++ Arrays and Hashing/347. Top K Frequent Elements/maxHeap/Solution.py
@@ -1,14 +1,14 @@
 # https://leetcode.com/problems/top-k-frequent-elements/description/
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_dict = collections.Counter(nums)
-        count_list = []
-        for key in count_dict:
-            count_list.append([count_dict[key]*-1, key])
-        heapq.heapify(count_list)
-        i =0 
-        ret = []
-        while i<k:
-            ret.append(heapq.heappop(count_list)[1])
-            i+=1
-        return ret
+        nums_dict = {}
+        for num in nums:
+            nums_dict[num] = nums_dict.get(num, 0) + 1
+        minHeap = []
+        for num in nums_dict:
+            if len(minHeap) >= k:
+                heapq.heappushpop(minHeap, (nums_dict[num], num))
+            else:
+                heapq.heappush(minHeap, (nums_dict[num], num))
+        return [key for val, key in minHeap]
