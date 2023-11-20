8fffcc687d52e4ea782b8f14c20eca4de32ea78f
Amit Bhadra <amitbhadra7@gmail.com>
2023-11-19 23:02:54 -0800

    11_19_2023

--- Binary Search/240. Search a 2D Matrix II/Binary Search/Solution.py
+++ Binary Search/240. Search a 2D Matrix II/Binary Search/Solution.py
@@ -1,22 +1,17 @@
 # https://leetcode.com/problems/search-a-2d-matrix-ii/description/
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def binary_search(arr):
-            i = 0 
-            j = len(arr)-1
-            while i<=j:
-                mid = (i+j)//2
-                if arr[mid]==target:
-                    return True
-                elif arr[mid]>target:
-                    j = mid-1
-                else:
-                    i = mid+1
-            return False
-        for i in range(len(matrix)):
-            if matrix[i][0]<=target<=matrix[i][-1]:
-                if binary_search(matrix[i]):
-                    return True
-            elif matrix[i][0]>target:
-                return False
+    def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
+        m=len(mat)
+        n=len(mat[0])
+        i=m-1
+        j=0
+        
+        while i>=0 and j<n:
+            if mat[i][j]==target:
+                return True
+            elif mat[i][j]<target:
+                j+=1
+            else:
+                i-=1
+                
         return False
